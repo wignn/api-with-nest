4addad3e452a822a204c87a72a5dba0b250d53d5
wignn <tigfiragnafatur1933@gmail.com>
2024-10-24 06:41:47 +0700

    Refactor UserController tests: Remove unnecessary test case
    The commit removes an unnecessary test case from the UserController tests. The test case was for the POST /api/books endpoint and was checking for a 401 status code. However, this test case is not needed as the endpoint now returns a 400 status code for invalid requests. This commit improves the test suite by removing redundant tests.

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -94,20 +94,4 @@ describe('UserController', () => {
     });
   });
   
-  describe('POST /api/books', () => {
-    afterEach(async () => {
-      await testService.deletebook();
-    });
-    it('should be rejected with 401', async () => {
-      const response = await request(app.getHttpServer()).post('/books').send({
-        title: 'test',
-        description: 'test',
-        autho: 'test',
-      });
-
-      logger.info(response.body);
-      expect(response.status).toBe(400);
-      expect(response.body.message).toBeDefined();
-    });
-  });
 });
